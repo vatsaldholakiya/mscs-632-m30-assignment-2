7192219dc42fae7eca59bd8a146e08731a1eda59
Vatsal Dholakiya <vatsaldholakiya97@gmail.com>
2026-09-13 11:38:08 -0500

    Create section-1.1-javascript-syntax-error-1.js

---
+++ section-1.1-javascript-syntax-error-1.js
@@ -0,0 +1,11 @@
+function calculateSum(arr) {
+    let total = 0;
+    for (let num of arr) {
+        total += num;
+    }
+    return total;
+}
+ 
+let numbers = [1, 2, 3, 4, 5;
+let result = calculateSum (numbers);
+console.log("Sum in JavaScript:", result);
