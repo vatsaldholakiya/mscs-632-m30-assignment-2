40d74744b31853ac27fed2229ddf401073713521
Vatsal Dholakiya <vatsaldholakiya97@gmail.com>
2026-09-14 17:06:11 -0500

    Create 2.js

---
+++ type-systems/javascript/2.js
@@ -0,0 +1,30 @@
+class Landmark { 
+  report() { 
+    return "?"; 
+  } 
+}
+class Peak extends Landmark {
+  constructor(name, m) { 
+    super(); 
+    this.name = name; 
+    this.m = m; 
+  }
+  
+  report() { 
+    return `${this.name} ${this.m}m`; 
+  }
+}
+const announce = l => console.log(l.report());
+
+
+
+const gear = ["rope"];
+const partner = gear;
+partner.push("axe");
+console.log(gear.length, partner.length);
+
+const k2 = new Peak("K2", 8611);
+announce(k2);
+announce({ report: () => "Baltoro 4000m" });
+console.log(k2.m / 4);
+console.log(2 ** 70, 2n ** 70n);
