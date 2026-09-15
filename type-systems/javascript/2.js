67d3f35b99388777b2ad1b6753afe173d0e56fff
Vatsal Dholakiya <vatsaldholakiya97@gmail.com>
2026-09-14 20:10:30 -0500

    Update 2.js

--- type-systems/javascript/2.js
+++ type-systems/javascript/2.js
@@ -4,5 +4,6 @@ partner.push("axe");
 console.log(gear.length, partner.length);
 
 console.log(8611 / 4);
-console.log(2 ** 70, 2n ** 70n);
+console.log(2**53 === 2**53+1)
+console.log(2n ** 70n);
 console.log(2n + 1)
