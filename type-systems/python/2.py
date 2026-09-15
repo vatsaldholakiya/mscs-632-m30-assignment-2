6928cc4c5e50af0c47e7879edb554c7c466f7b8d
Vatsal Dholakiya <vatsaldholakiya97@gmail.com>
2026-09-14 19:48:33 -0500

    Update 2.py

--- type-systems/python/2.py
+++ type-systems/python/2.py
@@ -1,24 +1,7 @@
-class Landmark:
-    def report(self): return "?"
-
-class Peak(Landmark):
-    def __init__(self, name, m): 
-        self.name, self.m = name, m
-    def report(self): 
-        return f"{self.name} {self.m}m"
-
-class Glacier:
-    def report(self): return "Baltoro 4000m"
-
-def announce(l): print(l.report())
-
 gear = ["rope"]
 partner = gear
 partner.append("axe")
 print(len(gear), len(partner))
 
-k2 = Peak("K2", 8611)
-announce(k2); 
-announce(Glacier())
-print(k2.m / 4, k2.m // 4)
+print(8211 / 4, 8211 // 4)
 print(2 ** 70)
