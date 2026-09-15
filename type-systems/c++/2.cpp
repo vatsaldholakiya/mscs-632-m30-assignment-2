10546738cacbb0ebacb4b5f6907e5a53fee0bccb
Vatsal Dholakiya <vatsaldholakiya97@gmail.com>
2026-09-14 19:48:57 -0500

    Update 2.cpp

--- type-systems/c++/2.cpp
+++ type-systems/c++/2.cpp
@@ -3,39 +3,14 @@
 #include <string>
 using namespace std;
 
-struct Landmark { 
-    virtual string report() const = 0; 
-};
-
-struct Peak : Landmark {                                  
-    string name; int m;
-    Peak(string n, int h) : name(n), m(h) {}
-    string report() const override { 
-        return name + " " + to_string(m) + "m"; 
-    }
-};
-
-struct Glacier { 
-    string report() const { 
-        return "Baltoro 4000m"; 
-    } 
-};   
-
-template <class T> void announce(const T& t) { 
-    cout<<t.report()<<"\n"; 
-}
 
 int main() {
     vector<string> gear{"rope"};
     auto partner = gear;                
     partner.push_back("axe");
-    cout<<gear.size()<<partner.size()<<"\n";   
-
-    Peak k2{"K2", 8611};
-    announce(k2); 
-    announce(Glacier{});  
+    cout<<gear.size()<<partner.size()<<"\n";    
     
-    cout<<k2.m / 4<<"\n";      
+    cout<<8611 / 4<<"\n";      
     unsigned long long flakes = 1;
     for (int h = 0; h < 70; ++h) {
         flakes *= 2;
